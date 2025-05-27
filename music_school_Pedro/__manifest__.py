caced8f97696dd40f8185e5f75edb24996568eb3
PedroRecio <pedrorecior@gmail.com>
2025-05-27 16:14:28 +0000

    modulo

---
+++ music_school_Pedro/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Music School',
+    'version': '18.0.0.0.0',
+    'summary': 'Manages a music school',
+    'description': 'Manage a music school',
+    'author': 'Pedro Recio',
+     'license': 'LGPL-3',
+    'category': 'Music School',
+    'depends': [
+        'base',
+    ],
+      'data':[
+        'security/res_groups.xml',
+        'security/ir.model.access.csv',
+        'views/music_school_student_views.xml',
+        'views/music_school_instrument_views.xml',
+        'views/music_school_menuitems.xml',
+    ]
+    
+}
